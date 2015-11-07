8bcc2f0097aff4d7b6ab3f9c42a92933e26c91a8
Chris <chris@thefoodtrace.com>
2015-11-07 15:21:15 -0600

    Core functions added

---
+++ X2/app/src/main/java/com/example/scoot/x2/Create.java
@@ -0,0 +1,16 @@
+package com.example.scoot.x2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Scoot on 11/7/2015.
+ */
+public class Create extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create);
+    }
+}
