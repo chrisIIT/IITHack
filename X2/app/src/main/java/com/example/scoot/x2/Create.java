fac854464fd708b4f14bbde59ccd5d10061f2037
usherman678 <usherman678@aol.com>
2015-11-08 00:06:20 -0600

    debugging starts

--- X2/app/src/main/java/com/example/scoot/x2/Create.java
+++ X2/app/src/main/java/com/example/scoot/x2/Create.java
@@ -39,6 +39,7 @@ public class Create extends Activity {
         datex = date.getText().toString();
         location = (EditText) findViewById(R.id.locID);
         locationx = location.getText().toString();
+        System.out.println(namex+" "+descriptionx);
     }
 
     //when done button is pressed goto events
@@ -50,20 +51,27 @@ public class Create extends Activity {
             setStrings(); //Needs to be here to make sure strings are gotten
             Intent i = new Intent(getApplicationContext(), MainActivity.class);
             //i.putExtra("newEID", eventID);
-            i.putExtra("rendezvous",new Rendezvous(namex,descriptionx,datex,locationx)); //better way then previous
+            i.putExtra("rendezvous", new Rendezvous(namex, descriptionx, datex, locationx)); //better way then previous
             Rendezvous r = new Rendezvous(namex,descriptionx,datex,locationx); //create new Rendezvous object - old won't do
             String oID = Integer.toString(r.getID());
-            //System.out.println(r);
+            System.out.println(r);
             RelativeLayout mRelativeLayout = (RelativeLayout)findViewById(R.id.relativeID);
             EditText e1 = new EditText(this);
             e1.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            e1.setText("Event"+" "+oID);
+            e1.setText("Event" + " " + oID);
+            System.out.println("just checking");
             mRelativeLayout.addView(e1);
+            System.out.println("here");
             e1.setWidth(200);
+            System.out.println("next");
             e1.setHeight(200);
+            System.out.println("that");
             e1.setX(500);
+            System.out.println("there");
             e1.setY(900);
+            System.out.println("what");
             startActivity(i);
+            System.out.println("you suck");
         }
 
     }
